11ab2bb6c5690fea8d0f1d52d9a3b6a389b6bdb6
Thomas Entholzner <tom.entholzner@gmail.com>
2015-08-21 14:28:53 +0000

    Added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMAiler < ActionMailer::Base
+    default to: 'thomas@entholzner.com'
+
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Conatct Form Message')
+    end
+end
